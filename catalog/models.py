a2c9d75f7da0e1fe6a0acc9bad286390be4ebb7e
Tatiyana Galitskaya <tatianagalitskaya3@gmail>
2024-06-19 19:46:49 +0300

    20.1

--- catalog/models.py
+++ catalog/models.py
@@ -1,3 +1,57 @@
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    category_name = models.CharField(max_length=50, verbose_name="Наименование")
+    category_description = models.TextField(verbose_name="Описание")
+
+    def __str__(self):
+        return self.category_name
+
+    class Meta:
+        verbose_name = "Категория"
+        verbose_name_plural = "Категории"
+
+
+class Product(models.Model):
+    product_name = models.CharField(
+        max_length=100,
+        verbose_name="Наименование",
+        help_text="Введите наименование " "продукта",
+    )
+    product_description = models.TextField(
+        verbose_name="Описание", help_text="Введите описание продукта"
+    )
+    product_picture = models.ImageField(
+        upload_to="picture_product/",
+        verbose_name="Изображение",
+        help_text="Загрузите фото продукта",
+        null=True,
+        blank=True,
+    )
+    product_category = models.ForeignKey(
+        Category,
+        on_delete=models.SET_NULL,
+        verbose_name="Категория",
+        help_text="Введите категорию продукта",
+        null=True,
+        blank=True,
+    )
+    price = models.DecimalField(
+        max_digits=10,
+        decimal_places=2,
+        verbose_name="Цена",
+        help_text="Введите цену",
+    )
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name="Дата создания")
+    updated_at = models.DateTimeField(
+        auto_now=True, verbose_name="Дата последнего изменения"
+    )
+
+    def __str__(self):
+        return f"{self.product_name}: {self.price}"
+
+    class Meta:
+        verbose_name = "Продукт"
+        verbose_name_plural = "Продукты"
+        ordering = ["product_name", "product_category", "price"]
--- catalog/urls.py
+++ catalog/urls.py
@@ -1,10 +1,10 @@
-from django.urls import path, include
-from catalog.apps import CatalogConfig
+from django.urls import path
+
 from catalog.views import home, contacts
 
-app_name = CatalogConfig.name
+app_name = "catalog"
 
 urlpatterns = [
-    path('', home, name='home'),
-    path('contacts/', contacts, name='contacts')
+    path("", home, name="home"),
+    path("contacts/", contacts, name="contacts"),
 ]
